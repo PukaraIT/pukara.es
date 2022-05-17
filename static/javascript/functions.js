905529573eff81ae64395cb02981cfae3fb27eaa
Eduardo Mendez <eduardo@pukara.es>
2022-05-17 10:20:13 +0200

    Basic website structure based in Hugo

---
+++ static/javascript/functions.js
@@ -0,0 +1,50 @@
+const slider = document.querySelectorAll('.dots');
+const panels = document.querySelectorAll('.slider');
+
+let interval;
+let regex = /[0-9]/g;
+let i = 0;
+let number = 0;
+
+const panelShow = (event) => {
+  if (event != undefined) {
+    number = event.target.id.match(regex);
+    clearInterval(interval);
+    interval = setInterval(panelShow, 5000);
+  }
+
+  while (i < panels.length) {
+    if (panels[i].getAttribute('class').match('show-slider')) {
+      panels[i].classList.toggle('show-slider');
+      panels[i].classList.toggle('hide-slider');
+      slider[i].classList.toggle('dots-active');
+      if (event == undefined) number = i;
+    }
+    i++;
+  }
+
+  i = 0;
+
+  if(event == undefined && number >= panels.length - 1) number = -1;
+
+  slider[event != undefined ? number - 1 : number + 1].classList.toggle('dots-active');
+  panels[event != undefined ? number - 1 : number + 1].classList.toggle('hide-slider');
+  panels[event != undefined ? number - 1 : number + 1].classList.toggle('show-slider');
+}
+
+while (i < slider.length) {
+  if (i == 0) {
+    panels[i].classList.toggle('show-slider');
+    panels[i].classList.toggle('hide-slider');
+    slider[i].classList.toggle('dots-active');
+  }
+
+  panels[i].setAttribute('id', 'panel-' + [i + 1])
+  slider[i].setAttribute('id', 'dot-' + [i + 1]);
+  slider[i].addEventListener('click', panelShow);
+  i++;
+}
+
+i = 0;
+
+interval = setInterval(panelShow, 5000);
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    "layouts/partials/**/*.html",
+    "layouts/*.html"
+  ],
+  mode: "jit",
+  theme: {
+    extend: {
+      fontFamily: {
+        'mulish': 'Mulish',
+        'eight-one': 'Eight One'
+      },
+      backgroundImage: {
+        'header': "url('../images/bg-banner.png')",
+        'hero': "url('../images/bg-hero.png')",
+        'footer': "url('../images/bg-footer.png')"
+      },
+      colors: {
+        pukara: {
+          main: '#F3D02B',
+          primary: '#E38718',
+          second: '#1D2426'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
