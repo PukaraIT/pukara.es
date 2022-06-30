815e83659e6227d1e51cd3c29fb992109862830c
GonzaEnrique27 <gonz_1994@hotmail.com>
2022-06-30 11:12:46 -0300

    Application of validations in form

---
+++ static/javascript/validationsForm.js
@@ -0,0 +1,30 @@
+const formulario = document.querySelector('#form'); 
+const inputs = document.querySelectorAll('#form input'); 
+
+
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const elements = e.target.elements;
+  let error = false;
+
+  for (let i = 0; i < elements.length -3; i++) {
+  if (elements[i].value === ""){
+    error = true
+    
+  }
+  
+}
+
+  const terminos = document.getElementById('notificaciones');
+
+  if(!terminos.checked){
+    document.querySelector('.input-checkbox .error').innerHTML = 'Debe aceptar los términos y condiciones.'
+    document.querySelector('.input-checkbox .error').style.backgroundColor = "yellow"
+    document.querySelector('.input-form-name .error').innerHTML = 'Debe ingresar un nombre.'
+    document.querySelector('.input-form-name .error').style.backgroundColor = "yellow"
+    document.querySelector('.input-form-email .error').innerHTML = 'Debe ingresar un correo valido.'
+    document.querySelector('.input-form-email .error').style.backgroundColor = "yellow"
+  } else if (terminos.checked){
+    formulario.submit();     
+  }         
+})
