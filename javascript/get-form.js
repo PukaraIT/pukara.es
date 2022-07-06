d42bbfc1f5ed1bea94c9ea13ae423be18b2ae7f4
njrzr <njrzr@users.noreply.github.com>
2022-07-06 21:00:14 +0000

    deploy: ef36eb86529c29fed60daa0aa5039a4139c43f40

--- javascript/get-form.js
+++ javascript/get-form.js
@@ -1,4 +1,4 @@
-fetch('https://api.getform.io/v1/forms/8f1a51cc-33d8-4d63-b3d7-14dd95fe572c?token=2WHzGpNilmOa0uNz7AcWOpOYMVWWwf4HdgqsCzh9XvoFgu3lr9eF8Z4WaTfr',{
+fetch('https://api.getform.io/v1/forms/8f1a51cc-33d8-4d63-b3d7-14dd95fe572c?token=5AABxHlI1BU62O7VAMQEVBs0F8zcVar9BK3lwLcHBjRAAzX80wK0HzEEtj2t',{
     message: 'New Contact Request from PK',
   })
   .then(response => response.json())
--- javascript/validationsForm.js
+++ javascript/validationsForm.js
@@ -1,27 +1,33 @@
 const formulario = document.querySelector('#form');
 const inputs = document.querySelectorAll('#form input');
 
-formulario.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const elements = e.target.elements;
-  let error = false;
+formulario.addEventListener("submit", onClick)
 
-  for (let i = 0; i < elements.length -3; i++) {
-  if (elements[i].value === ""){
-    error = true   
-  }
+function onClick(e) {
+  e.preventDefault();
+  grecaptcha.ready(function() {
+    grecaptcha.execute('6LeXbrIgAAAAAEv6HXO2F46atp5-3HiAGjmd9zTg', {action: 'submit'}).then(function(token) {
+        const elements = e.target.elements;
+        let error = false;
+      
+        for (let i = 0; i < elements.length -3; i++) {
+        if (elements[i].value === ""){
+          error = true   
+        }
+      }
+        const terminos = document.getElementById('notificaciones');
+        if(!terminos.checked){
+          document.querySelector('.input-checkbox .error').innerHTML = 'Debe aceptar los términos y condiciones.'
+          document.querySelector('.input-checkbox .error').style.backgroundColor = "yellow"
+          document.querySelector('.input-form-name .error').innerHTML = 'Debe ingresar un nombre.'
+          document.querySelector('.input-form-name .error').style.backgroundColor = "yellow"
+          document.querySelector('.input-form-email .error').innerHTML = 'Debe ingresar un correo valido.'
+          document.querySelector('.input-form-email .error').style.backgroundColor = "yellow"
+        } else if (terminos.checked){
+          formulario.submit();
+        }         
+    });
+  });
 }
 
-  const terminos = document.getElementById('notificaciones');
-
-  if(!terminos.checked){
-    document.querySelector('.input-checkbox .error').innerHTML = 'Debe aceptar los términos y condiciones.'
-    document.querySelector('.input-checkbox .error').style.backgroundColor = "yellow"
-    document.querySelector('.input-form-name .error').innerHTML = 'Debe ingresar un nombre.'
-    document.querySelector('.input-form-name .error').style.backgroundColor = "yellow"
-    document.querySelector('.input-form-email .error').innerHTML = 'Debe ingresar un correo valido.'
-    document.querySelector('.input-form-email .error').style.backgroundColor = "yellow"
-  } else if (terminos.checked){
-    formulario.submit();
-  }         
-})
+console.log(formulario)
