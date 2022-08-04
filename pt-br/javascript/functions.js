38e8bc7a96603ac71c763bcc4c168c6894691d47
eddymdz <eddymdz@users.noreply.github.com>
2022-08-04 11:43:18 +0000

    deploy: 7600baec65da082ec66cfa66d138fca92a5c7cbe

---
+++ pt-br/javascript/functions.js
@@ -0,0 +1,33 @@
+const panels = document.querySelectorAll('.slider');
+
+let interval;
+let regex = /[0-9]/g;
+let i = 0;
+let number = 0;
+
+const panelShow = (event) => {
+  if (event != undefined) {
+    number = event.target.id.match(regex);
+    clearInterval(interval);
+    interval = setInterval(panelShow, 5000);
+  }
+
+  while (i < panels.length) {
+    if (panels[i].getAttribute('class').match('show-slider')) {
+      panels[i].classList.toggle('show-slider');
+      panels[i].classList.toggle('hide-slider');
+      if (event == undefined) number = i;
+    }
+    i++;
+  }
+
+  i = 0;
+}
+
+interval = setInterval(panelShow, 5000);
+
+let video = document.querySelector('#puk-video');
+
+window.addEventListener('load',() => {
+  video.classList.toggle('invisible');
+});
